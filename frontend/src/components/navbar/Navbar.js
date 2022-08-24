8689e7ffceb281a07977584c36a4e669262f8d81
Simon-157 <boatengsimonjnr157@gmail.com>
2022-08-24 15:26:47 +0000

    navbar setup

---
+++ frontend/src/components/navbar/Navbar.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import navbarStyles from "./navbar.module.scss"
+
+const Navbar = () => {
+  return (
+  
+    <div className={navbarStyles}>
+        <nav>
+            <ul>
+
+            </ul>
+        </nav>
+    </div>
+  )
+}
+
+export default Navbar
