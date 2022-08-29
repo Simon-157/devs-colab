b71920b87d24cd2b5dd4d0fa4eedf381cab0fed9
Simon-157 <boatengsimonjnr157@gmail.com>
2022-08-29 23:27:51 +0000

    videostreaming set up

---
+++ frontend/src/components/video/Stream.js
@@ -0,0 +1,13 @@
+import {useState } from "react"
+const Stream = () => {
+    const [peers, setPeers] = useState({})
+    const [videos, setVideos] = useState({})
+
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default Stream;
