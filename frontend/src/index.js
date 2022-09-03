dc5b05c6636836bb944836f8fb8939d2215593fe
Simon-157 <boatengsimonjnr157@gmail.com>
2022-09-03 20:02:40 +0000

    initialized react query

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,6 +1,8 @@
 //libraries
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import  { QueryClientProvider, QueryClient } from "react-query"
+import { ReactQueryDevtools } from 'react-query/devtools'
 
 //styles
 import "./global/global.scss";
@@ -10,11 +12,15 @@ import "./index.css"
 import App from './App';
 import ErrorBoundary from './pages/errors/error-boundary/ErrorBoundary';
 
+const queryClient = new QueryClient();
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <ErrorBoundary>
-      <App />
+      <QueryClientProvider client ={queryClient} >
+        <App />
+        <ReactQueryDevtools />
+      </QueryClientProvider>
     </ErrorBoundary>
   </React.StrictMode>
 );
