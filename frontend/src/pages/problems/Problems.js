db2b1ff2aa7c70c88c580ee73866f401f2dc1f5e
Simon-157 <boatengsimonjnr157@gmail.com>
2022-09-13 14:06:12 +0000

    minor footer style edit

--- frontend/src/pages/problems/Problems.js
+++ frontend/src/pages/problems/Problems.js
@@ -4,14 +4,17 @@ import Challenges from './Challenges'
 import problemStyles from "./problem-styles.module.scss"
 
 const Problems = (props) => {
-
+  const date = new Date();
   return (
-    <div className={problemStyles.container}>
-        <SideBar />
-       <div className = {problemStyles.problemsDiv}>
-           <Challenges />
-       </div>
-    </div>
+    <><div className={problemStyles.container}>
+      <SideBar />
+      <div className={problemStyles.problemsDiv}>
+        <Challenges />
+      </div>
+    </div><div className="mt-8 p-4" style={{ position:"",color: "rgb(143, 189, 142) ", backgroundColor: "rgba(0, 0, 0, 0.2)" }}>
+        @{date.getUTCFullYear()}
+        <a href="https://tailwind-elements.com/"> Simon</a>
+      </div></>
   )
 }
 
